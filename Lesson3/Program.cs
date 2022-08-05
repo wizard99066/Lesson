c82009f7f211e693fd3115dae9817f0c5717a5f7
rita <ritavlas@yandex.ru>
2022-08-05 11:23:17 +0300

    Добавьте файлы проекта.

---
+++ Lesson3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int firstNumber = 9;
+int secondNumber = 12;
+Console.WriteLine( $"первое и второе число изначально {firstNumber}  и  {secondNumber}");
+Console.WriteLine("поменяем числа местами");
+firstNumber =firstNumber +secondNumber;
+secondNumber = firstNumber - secondNumber;
+firstNumber =  firstNumber-secondNumber;
+Console.Write($"первое и второе число после перестановки {firstNumber}  и  {secondNumber}");
