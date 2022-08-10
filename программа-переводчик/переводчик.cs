f31cd98538f27e11aaec5d9f2cf9ee1fc762379a
rita <ritavlas@yandex.ru>
2022-08-10 16:08:01 +0300

    создание программы- переводчика

---
+++ программа-переводчик/переводчик.cs
@@ -0,0 +1,33 @@
+﻿namespace программа_переводчик
+{
+    internal class переводчик
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, string>()
+            {
+                ["passion"] = "страсть",
+                ["smile"] = "улыбка",
+                ["eternity"] = "вечность",
+                ["fantastic"]="фантастический",
+                ["liberty"]="свобода",
+                ["tranquility"]="спкойствие",
+                ["love"]="любовь",
+                ["mother"]="мама",
+                ["asphodel"]="нарцисс",
+                ["fawn"]="олененок"                     
+               };
+            while (true)
+            {
+                Console.WriteLine("введите слово на английском");
+                var input = Console.ReadLine();
+                Console.WriteLine(dictionary.ContainsKey(input) ? dictionary[input] : "такого слова нет в словаре");
+                //if (dictionary.ContainsKey(input))                
+                //    Console.WriteLine(dictionary[input]);                
+                //else
+                //    Console.WriteLine("такого слова нет в словаре");
+            }
+
+        }
+    }
+}
